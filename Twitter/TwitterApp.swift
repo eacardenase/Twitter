ed86d92a87a4232dc83a5504f67bf7e321d97b18
Edwin Cardenas <eacardenase@gmail.com>
2026-06-05 18:32:21 -0500

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Edwin Cardenas on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
