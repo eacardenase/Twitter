d6ef7c23764bccdac27b9c21b825651607112164
Edwin Cardenas <eacardenase@gmail.com>
2026-06-05 18:37:37 -0500

    test: add unit test target and initial test zero

---
+++ TwitterTests/TwitterTests.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterTests.swift
+//  TwitterTests
+//
+//  Created by Edwin Cardenas on 6/5/26.
+//
+
+import XCTest
+
+class TwitterTests: XCTestCase {
+    func test_zero() {
+        XCTFail("Tests not yet implemented in TwitterTests")
+    }
+}
