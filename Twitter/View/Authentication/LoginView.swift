4ad01e0208034ab835f8bb764cd2febfe54e940a
Edwin Cardenas <eacardenase@gmail.com>
2026-07-08 16:13:06 -0500

    feat: finish up login view layout

--- Twitter/View/Authentication/LoginView.swift
+++ Twitter/View/Authentication/LoginView.swift
@@ -35,9 +35,49 @@ struct LoginView: View {
                         image: Image(systemName: "lock"),
                         isSecure: true
                     )
+
+                    HStack {
+                        Spacer()
+
+                        Button {
+                            // TODO: Implement Action
+                        } label: {
+                            Text("Forgot Password?")
+                                .foregroundStyle(.white)
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                        }
+                    }
+
+                    Button {
+                        // TODO: Implement Action
+                    } label: {
+                        Text("Sign In")
+                            .tint(.twitterBlue)
+                            .frame(maxWidth: .infinity)
+                            .font(.headline)
+                            .padding(.vertical)
+                            .background(.white)
+                            .clipShape(.capsule)
+
+                    }
                 }
+                .padding(.horizontal)
 
                 Spacer()
+
+                Button {
+                    // TODO: Implement Action
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("Don't have an account?")
+
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.callout)
+                }
             }
             .padding()
         }
