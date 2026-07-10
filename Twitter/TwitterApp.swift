caa7fade33dac0c014a887ff0afff4f529597496
Edwin Cardenas <eacardenase@gmail.com>
2026-07-10 11:59:57 -0500

    feat: add and configure firebase dependencies

--- Twitter/TwitterApp.swift
+++ Twitter/TwitterApp.swift
@@ -5,15 +5,29 @@
 //  Created by Edwin Cardenas on 6/5/26.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct TwitterApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State private var router = Router()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
                 .environment(router)
         }
     }
