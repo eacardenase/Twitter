9063849a83d857e88070bdd738dfc961e552e059
Edwin Cardenas <eacardenase@gmail.com>
2026-07-16 19:33:46 -0500

    refactor: extract tab view from content view

--- Twitter/ContentView.swift
+++ Twitter/ContentView.swift
@@ -12,61 +12,20 @@ struct ContentView: View {
     @Environment(AuthViewModel.self) private var viewModel
 
     var body: some View {
-        @Bindable var router = router
+        ZStack {
+            if viewModel.user != nil {
+                AppView(router: router)
+            } else {
+                LoginView()
+            }
 
-        Group {
             if viewModel.isLoading {
                 LaunchScreen()
                     .ignoresSafeArea()
-            } else if viewModel.user != nil {
-                TabView(selection: $router.selectedTab) {
-                    Tab("Home", systemImage: "house", value: .home) {
-                        NavigationStack(path: $router.homePath) {
-                            FeedView()
-                        }
-                    }
-
-                    Tab(
-                        "Search",
-                        systemImage: "magnifyingglass",
-                        value: .search
-                    ) {
-                        NavigationStack(path: $router.searchPath) {
-                            SearchView()
-                                .navigationDestination(for: Route.self) {
-                                    route in
-                                    routeView(route)
-                                }
-                        }
-                    }
-
-                    Tab("Messages", systemImage: "envelope", value: .messages) {
-                        NavigationStack(path: $router.messagesPath) {
-                            ConversationsView()
-                                .navigationDestination(for: Route.self) {
-                                    route in
-                                    routeView(route)
-                                }
-                        }
-                    }
-                }
-                .tint(.twitterBlue)
-            } else {
-                LoginView()
             }
         }
         .task {
             await viewModel.verifyLogin()
-        }
-    }
-
-    @ViewBuilder
-    private func routeView(_ route: Route) -> some View {
-        switch route {
-        case .conversation(let conversation):
-            ChatView(conversation: conversation)
-        case .profile(let user):
-            ProfileView(user: user)
         }
     }
 }
